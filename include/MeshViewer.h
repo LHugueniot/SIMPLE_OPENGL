74d574f762886e3180dfcc932c65bb0f32721f5a
LHugueniot <lucien98@gmail.com>
2020-10-31 19:32:55 +0000

    Laptop to ubuntu cheesegrater

---
+++ include/MeshViewer.h
@@ -0,0 +1,30 @@
+#ifndef MESH_VIEWER_H
+#define MESH_VIEWER_H
+
+#include "Common.h"
+
+struct Mesh{
+
+    // Mesh name
+    std::string m_name;
+    // Vertex Array Object
+    GLuint m_vao;
+    // Vertex Buffer Object
+    GLuint m_vbo;
+    // Index Buffer Object
+    GLuint m_ibo;
+
+    // Total number of conceptual vertices
+    uint m_verticesNum;
+    // Total size of m_vertexPositions (m_verticesNum * 3)
+    uint m_verticesDataSize;
+    // Vertex data buffer
+    float * m_verticesData;
+
+    // Total number of elements for m_faceIndices
+    uint m_faceIndicesDataSize;
+    // Ptr to face indices data
+    uint * m_faceIndicesData;
+}
+
+#endif /* MESH_VIEWER_H */
---
+++ src/ParticleViewer.cpp
@@ -0,0 +1,1 @@
+#include "ParticleViewer.h"
